d5f88d8a18a6d2f8c6d3eee57ff0d6124ad27456
ClaudioNuccio <147241773+ClaudioNuccio@users.noreply.github.com>
2023-10-10 16:38:07 +0200

    Update Java_casting_2.java

--- Java_casting_2.java
+++ Java_casting_2.java
@@ -2,8 +2,8 @@ public class MyClass {
     public static void main(String args[]) {
       char m = 'm';
       char k = 'k';
-      String sumOfMk = (int) (m) + (int) (k) ;
+      double sumOfMk = (int) m  + (int) k;
 
-      System.out.println("The sum of the two characters is   " + sumOfMk);
+      System.out.println("The sum of the two characters in ASCII is  " + sumOfMk);
     }
 }
